019ceb59d3b393331a6b114864f2da3be41dc101
budmonde <budmonde@mit.edu>
2016-01-06 02:39:59 -0500

    App updated

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+
+// Database in Memory
 var db = {
 	'david': 'apple',
 	'monde': 'orange',
@@ -10,29 +12,50 @@ var db = {
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Your Favorite Fruit'});
+
+  // Rendering the index view with the title 'Sign Up'
+  res.render('index', { title: 'Sign Up'});
+
 });
 
 /* GET userlist JSON */
 router.get('/userlist', function (req, res, next) {
+
+	// Sending the db object
 	res.send(db);
+
 });
+
 /* POST to adduser */
 router.post('/adduser', function (req, res, next) {
-	var userName = req.body.username;
-	var userColor = req.body.usercolor;
 
-	db[userName] = userColor;
+	// Catching variables passed in the form
+	var userName = req.body.username;
+	var userFruit = req.body.userfruit;
+
+	// Adding the new entry to the db
+	db[userName] = userFruit;
+
+	// Redirecting back to the root
 	res.redirect('/');
+
 });
 
 /* POST to deleteuser */
 router.post('/deleteuser', function (req, res, next) {
+
+	// Catching variables passed in the form
 	var userName = req.body.username;
+
+	// Checking whether user is in db
 	if (userName in db) {
+
+		// If yes, user is deleted from db
 		delete db[userName]
 		res.redirect('/');	
 	} else {
+
+		// If not, error message is rendered
 		var err = {
 			message: "User not found",
 		};
@@ -40,5 +63,4 @@ router.post('/deleteuser', function (req, res, next) {
 	}
 });
 
-router.get
 module.exports = router;
